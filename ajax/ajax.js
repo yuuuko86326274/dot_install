093cb7df3853b9a5bedb17827fe3323c211b936d
yuuuko86326274 <yuko.67.53@gmail.com>
2021-04-07 23:13:55 +0900

    railsnew

---
+++ ajax/ajax.js
@@ -0,0 +1,36 @@
+// ページの一部だけをreloadする方法
+// Ajaxを使う方法
+// XMLHttpRequestを使ってAjaxで更新
+function ajaxUpdate(url, element) {
+
+  // urlを加工し、キャッシュされないurlにする。
+  url = url + '?ver=' + new Date().getTime();
+
+  // ajaxオブジェクト生成
+  var ajax = new XMLHttpRequest;
+
+  // ajax通信open
+  ajax.open('GET', url, true);
+
+  // ajax返信時の処理
+  ajax.onload = function () {
+
+      // ajax返信から得たHTMLでDOM要素を更新
+      element.innerHTML = ajax.responseText;
+  };
+
+  // ajax開始
+  ajax.send(null);
+}
+
+window.addEventListener('load', function () {
+
+  var url = "ajax.html";
+
+  var div = document.getElementById('ajaxreload');
+
+  setTimeout(function () {
+      ajaxUpdate(url, div);
+  }, 5000);
+
+});
