72b22d03f6c27541d0fae5e5a0214b0c1a0f1119
yuuuko86326274 <yuko.67.53@gmail.com>
2021-04-20 22:10:19 +0900

    js

---
+++ javascriptbasics/js/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+{
+  document.querySelector('button').addEventListener('click', () => {
+    const targetNode = document.getElementById('target');
+
+      // targetNode.textContent = 'Dotinstall';
+      targetNode.textContent = targetNode.dataset.translation;
+  });
+}
